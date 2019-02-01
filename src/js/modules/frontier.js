58a9ceb7beab15bdf97893ff06bf166edeb5905b
Andy Ball <work@andyball.info>
2019-02-01 15:11:12 +1100

    Add states navigation with zoom to bounding box feature

--- src/js/modules/frontier.js
+++ src/js/modules/frontier.js
@@ -21,7 +21,10 @@ import 'leaflet.markercluster'
 import '../modules/Leaflet.GoogleMutant.js'
 import * as topojson from "topojson"
 import share from '../modules/share'
+import australia from '../modules/states.json'
 import smoothscroll from 'smoothscroll-polyfill';
+import bbox from 'geojson-bbox';
+
 
 smoothscroll.polyfill();
 Ractive.DEBUG = false;
@@ -40,7 +43,7 @@ export class Frontier {
 
         this.screenHeight = document.documentElement.clientHeight
 
-        this.isMobile = this.mobileCheck()
+        this.isMobile = this.toolbelt.mobileCheck()
 
         this.nom = 5 // Number of masacres on radial search
 
@@ -85,7 +88,6 @@ export class Frontier {
           tag: "Colonial_Civilians",
           selected: true
         }]
-
 
         for (var i = 0; i < self.googledoc.length; i++) {
 
@@ -172,9 +174,9 @@ export class Frontier {
 
             filterSpecial: "",
 
-            proximity: (self.isMobile) ? true: false,
+            proximity: (self.isMobile || window.location.origin === "file://" || window.location.origin === null) ? true: false,
 
-            legend: (self.isMobile) ? true: false,
+            legend: (self.isMobile|| window.location.origin === "file://" || window.location.origin === null) ? true: false,
 
             isApp: (window.location.origin === "file://" || window.location.origin === null) ? true : false ,
 
@@ -213,20 +215,6 @@ export class Frontier {
         this.postcoder()
 
 	}
-
-    mobileCheck() {
-        var self = this
-        var check = false;
-        (function(a) {
-            if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true
-        })(navigator.userAgent || navigator.vendor || window.opera);
-        var isiPad = navigator.userAgent.match(/iPad/i) != null;
-        var isApp = (window.location.origin === "file://" || window.location.origin === "null") ? true : false ;
-        var preflight = (check || isiPad ? true : false)
-        var status = (isApp) ? true : preflight ;
-
-        return status;
-    }
 
     postcoder() {
 
@@ -320,14 +308,12 @@ export class Frontier {
 
         });
 
-
         this.ractive.observe('proximity', ( proximity ) => {
 
             self.database.proximity = proximity
 
             self.ractive.set(self.database)
             
-
             if (self.database.geocheck) {
 
                 self.geocheck()
@@ -368,7 +354,6 @@ export class Frontier {
 
         });
 
-
         this.ractive.on( 'close', function ( context ) {
 
             var target = document.getElementById("legend")
@@ -385,7 +370,6 @@ export class Frontier {
 
         });
 
-
         this.ractive.on( 'geo', function ( context ) {
 
             if (self.database.userLatitude!=null) {
@@ -399,7 +383,6 @@ export class Frontier {
             }
 
         });
-
 
         this.ractive.on( 'top', function ( context ) {
 
@@ -427,6 +410,251 @@ export class Frontier {
         
         this.googleizer()
 
+    }
+
+    googleizer() {
+
+        var self = this
+
+        GoogleMapsLoader.KEY = 'AIzaSyD8Op4vGvy_plVVJGjuC5r0ZbqmmoTOmKk';
+        GoogleMapsLoader.REGION = 'AU';
+        GoogleMapsLoader.load(function(google) {
+            self.initMap()
+        });
+
+    }
+
+    initMap() {
+
+        var self = this
+
+        this.map = new L.Map('map', { 
+            renderer: L.canvas(),
+            center: new L.LatLng(self.latitude, self.longitude), 
+            zoom: self.zoom,
+            scrollWheelZoom: false,
+            dragging: true,
+            zoomControl: true,
+            doubleClickZoom: true,
+            zoomAnimation: true
+        })
+
+        self.map.zoomControl.setPosition('topright');
+
+        var styled = L.gridLayer.googleMutant({
+
+            type: 'roadmap',
+
+            styles: mapstyles
+
+        }).addTo(self.map);
+
+        var geojson = L.geoJson(australia, {
+            style: {
+                weight: 1,
+                opacity: 1,
+                color: 'black',
+                fillOpacity: 0,
+            }
+        }).addTo(self.map);
+
+        // Set the circle radius depending on zoom level
+
+        self.map.on('zoomend', function(e) {
+
+            var radius = self.getRadius()
+    
+            for (var i = 0; i < self.array.length; i++) {
+
+                self.array[i].setRadius(radius);
+
+            }
+
+        });
+
+        this.map.on('click', function(e) {
+
+            self.database.radial = false
+
+            if (self.database.proximity) {
+
+                self.getClosest(e.latlng.lat, e.latlng.lng)
+
+            }
+
+        });
+
+        this.compile()
+
+        this.casualties()
+
+        this.topo()
+
+        this.appscroll()
+
+        this.createState()
+
+    }
+
+    compile() {
+
+        var self = this
+
+        var slider = document.getElementsByClassName('filter_slider')[0];
+
+        noUiSlider.create(slider, {
+            start: [1776, 1929],
+            connect: true,
+            range: {
+                'min': 1776,
+                'max': 1929
+            }
+        });
+
+        slider.noUiSlider.on('slide', function( values, handle, unencoded, tap, positions ) {
+
+            self.database.DateStart = parseInt(values[0])
+
+            self.database.DateEnd = parseInt(values[1])
+
+            self.getData().then( (data) => {
+
+                self.ractive.set(self.database)
+
+            })
+
+        });
+
+    }
+
+    casualties() {
+
+        var self = this
+
+        this.choice = new Choices(document.getElementById('casualties'), {
+
+            removeItemButton: true,
+
+        });
+
+        this.choice.setChoices(self.identities, 'value', 'label', false);
+
+        this.choice.passedElement.element.addEventListener('addItem', function(event) {
+
+            self.database.identities.push(+event.detail.value)
+
+                self.getData().then( (data) => {
+
+                    self.ractive.set(self.database)
+
+                })
+
+        });
+
+        this.choice.passedElement.element.addEventListener('removeItem', function(event) {
+
+            var index = self.database.identities.indexOf(+event.detail.value);
+
+            if (index !== -1) self.database.identities.splice(index, 1);
+
+            self.getData().then( (data) => {
+
+                self.ractive.set(self.database)
+
+            })
+
+        });
+
+
+    }
+
+    appscroll() {
+
+        var isAndroidApp = (window.location.origin === "file://" && /(android)/i.test(navigator.userAgent) ) ? true : false ;
+
+        var el = document.getElementById('map');
+
+        el.ontouchstart = function(e){
+
+            if (isAndroidApp && window.top.GuardianJSInterface.registerRelatedCardsTouch) {
+
+              window.top.GuardianJSInterface.registerRelatedCardsTouch(true);
+
+            }
+        };
+
+        el.ontouchend = function(e){
+
+            if (isAndroidApp && window.top.GuardianJSInterface.registerRelatedCardsTouch) {
+
+              window.top.GuardianJSInterface.registerRelatedCardsTouch(false);
+
+            }
+
+        };
+
+    }
+
+    createState() {
+
+        var self = this
+          
+        var states = L.control({ position: 'bottomright' });
+     
+        states.onAdd = function() {
+     
+            var leg = L.DomUtil.create('div', 'states');
+     
+            leg.innerHTML += '<ul>'; 
+         
+            leg.innerHTML += '<li data-sid="0">NSW</li>';
+
+            leg.innerHTML += '<li data-sid="1">VIC</li>';
+
+            leg.innerHTML += '<li data-sid="2">QLD</li>';
+
+            leg.innerHTML += '<li data-sid="3">SA</li>';
+
+            leg.innerHTML += '<li data-sid="4">WA</li>';
+
+            leg.innerHTML += '<li data-sid="5">TAS</li>';
+
+            leg.innerHTML += '<li data-sid="6">NT</li>';
+
+            leg.innerHTML += '<li data-sid="7">ACT</li>';
+
+            leg.innerHTML += '</ul>';
+     
+            return leg;
+        };
+     
+        states.addTo(self.map);
+
+        var lis = document.getElementsByClassName("states")[0].getElementsByTagName('li');
+
+        for (var i=0; i<lis.length; i++) {
+            lis[i].addEventListener('click', function() {
+
+                var extent = bbox(australia[0].features[+this.getAttribute("data-sid")]); 
+                var southWest = L.latLng(extent[1], extent[0]);
+                var northEast = L.latLng(extent[3], extent[2]);
+                var bounds = new L.LatLngBounds(southWest,northEast);
+
+                self.map.fitBounds(bounds);
+
+                var target = document.getElementById("legend")
+
+                if (!target.classList.contains('hideFilters')) {
+
+                    target.classList.add('hideFilters')
+
+                }
+
+
+
+            }, false);
+        }
+     
     }
 
     geocheck() {
@@ -495,88 +723,7 @@ export class Frontier {
 
         }
 
-        
-
         self.ractive.set(self.database)
-
-    }
-
-    googleizer() {
-
-        var self = this
-
-        GoogleMapsLoader.KEY = 'AIzaSyD8Op4vGvy_plVVJGjuC5r0ZbqmmoTOmKk';
-        GoogleMapsLoader.REGION = 'AU';
-        GoogleMapsLoader.load(function(google) {
-            self.initMap()
-        });
-
-    }
-
-    initMap() {
-
-        var self = this
-
-        this.map = new L.Map('map', { 
-            renderer: L.canvas(),
-            center: new L.LatLng(self.latitude, self.longitude), 
-            zoom: self.zoom,
-            scrollWheelZoom: false,
-            dragging: true,
-            zoomControl: true,
-            doubleClickZoom: true,
-            zoomAnimation: true
-        })
-
-        self.map.zoomControl.setPosition('topright');
-
-        var styled = L.gridLayer.googleMutant({
-
-            type: 'roadmap',
-
-            styles: mapstyles
-
-        }).addTo(self.map);
-
-        // Set the circle radius depending on zoom level
-
-        self.map.on('zoomend', function(e) {
-
-            var radius = self.getRadius()
-    
-            for (var i = 0; i < self.array.length; i++) {
-
-                self.array[i].setRadius(radius);
-
-            }
-
-        });
-
-        this.map.on('click', function(e) {
-
-            self.database.radial = false
-
-            if (self.database.proximity) {
-
-                self.getClosest(e.latlng.lat, e.latlng.lng)
-
-            } else {
-
-                //self.map.panTo(new L.LatLng(e.latlng.lat, e.latlng.lng));
-
-                //self.scrollTo($("#navbar"), 0)
-
-            }
-
-        });
-
-        this.compile()
-
-        this.casualties()
-
-        this.topo()
-
-        this.appscroll()
 
     }
 
@@ -585,8 +732,6 @@ export class Frontier {
         var self = this
 
         var currentZoom = self.map.getZoom()
-
-        //https://wiki.openstreetmap.org/wiki/Zoom_levels
 
         var radius =  (currentZoom === 0) ? 156412 :
          (currentZoom === 1) ? 78206 :
@@ -609,55 +754,9 @@ export class Frontier {
          (currentZoom === 18) ? 0.596  :
          (currentZoom === 19) ? 0.298  : 0.1 ;
 
-        /*
-        The distance represented by one pixel (S) is given by
-
-        S=C*cos(y)/2^(z+8) where...
-
-        C is the (equatorial) circumference of the Earth
-
-        z is the zoom level
-
-        y is the latitude of where you're interested in the scale.
-        */
-
-
         radius = radius * 5
 
-        //var metresPerPixel = 40075016.686 * Math.abs(Math.cos(self.map.getCenter().lat)) / Math.pow(2, (self.map.getZoom()+8));
-
-        //console.log(currentZoom + ' | ' + metresPerPixel)
-
-        //var radius = metresPerPixel * 4
-
         return radius
-
-    }
-
-    appscroll() {
-
-        var isAndroidApp = (window.location.origin === "file://" && /(android)/i.test(navigator.userAgent) ) ? true : false ;
-
-        var el = document.getElementById('map');
-
-        el.ontouchstart = function(e){
-
-            if (isAndroidApp && window.top.GuardianJSInterface.registerRelatedCardsTouch) {
-
-              window.top.GuardianJSInterface.registerRelatedCardsTouch(true);
-
-            }
-        };
-
-        el.ontouchend = function(e){
-
-            if (isAndroidApp && window.top.GuardianJSInterface.registerRelatedCardsTouch) {
-
-              window.top.GuardianJSInterface.registerRelatedCardsTouch(false);
-
-            }
-
-        };
 
     }
 
@@ -700,7 +799,6 @@ export class Frontier {
         self.ractive.set(self.database)
 
         self.renderCircles(lat, lng)
-
 
     }
 
@@ -815,12 +913,12 @@ export class Frontier {
 
         self.rads = L.featureGroup(array).addTo(self.map);
 
-
     }
 
     colourizer(num) {
-        return (num < 11) ? '1' :
-        (num < 21) ? '2' : '3' ;
+
+        return (num < 11) ? '1' : (num < 21) ? '2' : '3' ;
+
     }
 
     topo() {
@@ -831,17 +929,15 @@ export class Frontier {
 
         }
 
-
         function borderizer(border) {
 
             return (border) ? 0.5 : 0 ;
 
         }
 
-
         var self = this
 
-        if(self.map.hasLayer(self.massacres)) {
+        if (self.map.hasLayer(self.massacres)) {
 
             self.massacres.eachLayer(
                 function(l){ 
@@ -865,6 +961,7 @@ export class Frontier {
             });
 
             self.array.push(marker);
+
         }
 
         self.massacres = L.featureGroup(self.array).addTo(self.map);
@@ -883,7 +980,6 @@ export class Frontier {
             <strong>Aboriginal dead:</strong> {aboriginal}<br />
             <strong>Coloniser dead:</strong> {coloniser}<br /><br />
             <div class="readmore" data-id="{id}">Click to see full description</div>`;
-
 
         self.massacres.on('mouseover', (e) => {
 
@@ -906,10 +1002,8 @@ export class Frontier {
 
             var tooltipContent = L.Util.template(tooltipTemplate, tooltipData); 
 
-          var popup = L.popup()
-           .setLatLng(e.latlng) 
-           .setContent(tooltipContent)
-           .openOn(self.map);
+            var popup = L.popup().setLatLng(e.latlng).setContent(tooltipContent).openOn(self.map);
+
         });
 
         self.map.on('popupopen', function() {  
@@ -972,78 +1066,6 @@ export class Frontier {
 
     }
 
-    casualties() {
-
-        var self = this
-
-        this.choice = new Choices(document.getElementById('casualties'), {
-
-            removeItemButton: true,
-
-        });
-
-        this.choice.setChoices(self.identities, 'value', 'label', false);
-
-        this.choice.passedElement.element.addEventListener('addItem', function(event) {
-
-            self.database.identities.push(+event.detail.value)
-
-                self.getData().then( (data) => {
-
-                    self.ractive.set(self.database)
-
-                })
-
-        });
-
-        this.choice.passedElement.element.addEventListener('removeItem', function(event) {
-
-            var index = self.database.identities.indexOf(+event.detail.value);
-
-            if (index !== -1) self.database.identities.splice(index, 1);
-
-            self.getData().then( (data) => {
-
-                self.ractive.set(self.database)
-
-            })
-
-        });
-
-
-    }
-
-    compile() {
-
-        var self = this
-
-        var slider = document.getElementsByClassName('filter_slider')[0];
-
-        noUiSlider.create(slider, {
-            start: [1776, 1929],
-            connect: true,
-            range: {
-                'min': 1776,
-                'max': 1929
-            }
-        });
-
-        slider.noUiSlider.on('slide', function( values, handle, unencoded, tap, positions ) {
-
-            self.database.DateStart = parseInt(values[0])
-
-            self.database.DateEnd = parseInt(values[1])
-
-            self.getData().then( (data) => {
-
-                self.ractive.set(self.database)
-
-            })
-
-        });
-
-    }
-
     getData() {
 
         var self = this
@@ -1054,11 +1076,12 @@ export class Frontier {
             var data_one = self.googledoc.filter(function(item) {
 
                 if (item.year >= self.database.DateStart && item.year <= self.database.DateEnd) {
+
                     return item
+
                 }
 
             });
-
 
             // Set the range for the number of deaths
             var range = (self.database.filterDeaths === '6 to 10') ? [6,10] :
@@ -1068,34 +1091,36 @@ export class Frontier {
             var data_two = data_one.filter(function(item) {
 
                 if (item.total_dead >= range[0] && item.total_dead <= range[1]) {
+
                     return item
+
                 }
 
             })
-
 
             var data_three = data_two.filter(function(item) {
 
-                //returns true for 1 or more matches, where 'a' is an array and 'b' is a search string or an array of multiple search strings
                 if ( self.toolbelt.contains(item.identities, self.database.identities) ) {
+
                     return item
+
                 }
 
             })
-
 
             self.database.records = data_three
 
             self.topo()
 
             if (self.database.latitude!=null && self.database.longitude!=null && self.database.proximity) {
+
                 self.getClosest(self.database.latitude, self.database.longitude)
+
             }
 
             resolve(data_one);  
 
         })
-
 
     }
 
@@ -1133,9 +1158,7 @@ export class Frontier {
 
         };
 
-
     }
-
 
     scrollTo(element, time) {
 
